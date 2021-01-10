eadbfb062ca72d84298117477b5204612d3c9d8f
unknown <davilimarodrigues@gmail.com>
2021-01-10 00:40:37 -0300

    Project created

---
+++ backend/src/test/java/com/davisds2/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davisds2.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
